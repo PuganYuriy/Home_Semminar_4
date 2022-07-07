11e60d7953e9de03ddf72c661ac7c6c1321db627
YuriyP <solovej-1991@mail.ru>
2022-07-07 19:34:03 +0600

    Task №27

--- Program.cs
+++ Program.cs
@@ -25,8 +25,35 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+// void sum_number1 (int num_2) // создал метод "void"
+//     {
+//         // int num_3 = num_2;
+//         // int num_2a1 = num_3 % 10;
+//         // int num_2a2 = num_3 / 10 % 10;
+//         // int num_2a3 = num_3 / 100 % 10;
+//         // int num_2a4 = num_3 / 1000 % 10;
+//         // int num_2a5 = num_3 / 10000 % 10;
 
+//         //     Console.WriteLine (num_2a1 + num_2a2 + num_2a3 + num_2a4 + num_2a5);
+//     }
 
+void sum_number (int num_2) // создал метод "void"
+    {
+        int sum_num = 0; // создание переменной
+        int sum = 0; // создание переменной
+        for (int i = 1; i < num_2; i = i * 10) // цикл "for" 
+        {
+            sum_num = (num_2 / i % 10) + sum; // выводит остаток числа и складывает его с сохранненой переменной "sum"
+            sum = sum_num; // сохраняет в переменную полученный результат
+        }
+        Console.WriteLine (sum); // выводит на экран полученный результат
+    }
+
+Console.WriteLine("Введите целое число"); 
+int num_1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write (num_1 + " -> "); // выводит на консоль введенное число
+sum_number (num_1); // вызывает функцию и выводит на экран полученный результат
 
 
 
@@ -35,33 +62,33 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] Generate_random_array(int n, int start, int end)
-{
-    int[] RandomArray = new int [n];
-    for (int i = 0; i < n; i++)
-    {
-        RandomArray[i] = new Random().Next(start, end + 1);
-    }
-    return RandomArray;
-}
+// int[] Generate_random_array(int n, int start, int end)
+// {
+//     int[] RandomArray = new int [n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
 
-void Show_array(int [] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + ", ");
-    }
-    Console.WriteLine ( );
-}
+// void Show_array(int [] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + ", ");
+//     }
+//     Console.WriteLine ( );
+// }
 
-Console.WriteLine("Введите количество элементов в массиве"); 
-int num_1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите количество элементов в массиве"); 
+// int num_1 = Convert.ToInt32(Console.ReadLine()); 
 
-Console.WriteLine("Введите первое число случайно генирируемых числел"); 
-int num_min = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите первое число случайно генирируемых числел"); 
+// int num_min = Convert.ToInt32(Console.ReadLine()); 
 
-Console.WriteLine("Введите последнее число случайно генирируемых числел"); 
-int num_max = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите последнее число случайно генирируемых числел"); 
+// int num_max = Convert.ToInt32(Console.ReadLine()); 
 
-int [] array_random = Generate_random_array (num_1, num_min, num_max);
-Show_array(array_random);
+// int [] array_random = Generate_random_array (num_1, num_min, num_max);
+// Show_array(array_random);
